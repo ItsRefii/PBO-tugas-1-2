b50abd134f53e598863cba3e93c8ac4dd1007d41
ItsRefii <refiabdillah19@gmail.comm>
2025-09-01 19:10:14 +0700

    Menambahkan file DemoNilai.java

---
+++ DemoNilai.java
@@ -0,0 +1,6 @@
+public class DemoNilai {
+    public static void main(String[] args){
+        Nilai nilai = new Nilai("Refi Abdillah", "2109106043", 100, 96, 87, 93);
+        nilai.tampilkanInfo();
+}
+}
