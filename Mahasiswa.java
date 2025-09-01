453eb2059813909a5e12f2ebd45a1bd145fad3e3
ItsRefii <refiabdillah19@gmail.comm>
2025-09-01 18:54:58 +0700

    first commit

---
+++ Mahasiswa.java
@@ -0,0 +1,24 @@
+
+public class Mahasiswa {
+        String nama ;
+
+        
+        public Mahasiswa(String nama){
+            this.nama = nama;
+        }
+        
+        public void membaca(){
+            System.out.println(nama + " Sedang membaca buku");                                                                             
+        }
+        
+         public void nyontek(){
+            System.out.println(nama + " Sedang nyontek dari buku Afwan");                                                                             
+        }
+         
+         public void modifikasi(){
+            System.out.println(nama + " Sedang memodifikasi jawaban dari afwan");                                                                             
+        }
+         
+        
+        
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,9 @@
+public class MahasiswaBeraksi {
+    public static void main(String[] args){
+        Mahasiswa mahasiswa = new Mahasiswa("Refi Abdillah");
+        
+        mahasiswa.membaca();
+        mahasiswa.nyontek();
+        mahasiswa.modifikasi();
+    }   
+}
